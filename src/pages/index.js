5617f422497bf390687122688f8efecd573405cc
Akinwale Habib <akinwalehabib@hotmail.com>
2024-02-03 23:16:42 +0100

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,18 +15,18 @@ export default function Home() {
         <div className={styles.container}>
             <img src="img/akinwale-habib.jpeg" alt="Image of akinwale habib" className={styles.image} />
 
-            <div className={styles.herocontentcontainer}>
+            <div className={styles.contentcontainer}>
               <p className={clsx("hero__subtitle", styles.contentsubtitle)} >Hi 👋, I'm Akinwale Habib.</p>
               <p className={styles.contenttext}>
-                I am a senior software engineer with extensive experience in IT and software engineering, specializing in Java, Typescript, and JavaScript. I possess expertise in product engineering, IT service management and project management.
+                I am a senior software engineer with extensive experience in IT and software engineering, specializing in product engineering, IT service management and project management.
                 
                 <br /><br />
                 
-                I've contributed to the growth of software developers as a learning facilitator at Andela's coding bootcamp, showcasing my commitment to collaborative knowledge-sharing. I am eager to contribute my expertise and innovation-driven passion to a forward-thinking team/organisation. I freelance on the side, so feel free to reach out to me if you need help with building or improving your technical solutions.
+                I've contributed to the growth of software developers as a learning facilitator at Andela's coding bootcamp, showcasing my commitment to collaborative knowledge-sharing. I am eager to contribute my expertise and innovation-driven passion to a forward-thinking team/organisation. Feel free to reach out to me if you need help with building or improving your technical solutions.
 
                 <br /><br />
 
-                You can find on <a href='https://www.linkedin.com/in/akinwalehabib' target="_blank">LinkedIn</a>, <a href='https://www.x.com/@akinwalehabib' target="_blank">X</a>, <a href='https://www.github.com/akinwale-habibullah' target="_blank">Github</a>
+                You can find me on <a href='https://www.linkedin.com/in/akinwalehabib' target="_blank">LinkedIn</a>, <a href='https://www.x.com/@akinwalehabib' target="_blank">X</a>, <a href='https://www.github.com/akinwale-habibullah' target="_blank">Github</a>
               </p>
             </div>
         </div>
